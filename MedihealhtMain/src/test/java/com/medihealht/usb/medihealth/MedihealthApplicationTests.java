0f549023cb08bc2d1937a1353fc5db21015c8ae6
edwin <edwin@LAPTOP-EALUMJPG>
2023-05-24 06:57:34 -0500

    carga inicial

---
+++ MedihealhtMain/src/test/java/com/medihealht/usb/medihealth/MedihealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medihealht.usb.medihealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedihealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
